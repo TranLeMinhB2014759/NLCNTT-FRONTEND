f1b53552cbfc34f397439a14efe724e86f4711d9
TranLeMinhB2014759 <minhb2014759@student.ctu.edu.vn>
2024-02-26 23:12:46 +0700

    update

--- src/components/Base/Footer.vue
+++ src/components/Base/Footer.vue
@@ -10,7 +10,7 @@
         <!-- Grid row -->
         <div class="row mt-3">
           <!-- Grid column -->
-          <div class="col-4 mx-auto mb-4">
+          <div class="col-4 col-sm-4 mx-auto mb-4">
             <!-- Content -->
             <h6 class="text-uppercase fw-bold mb-4">
               DERMATOLOGY CLINIC
@@ -19,13 +19,10 @@
             <p>
               Địa chỉ: Thới Bình 1, phường Thuận An, quận Thốt Nốt, Cần Thơ
             </p>
-            <p>
-              Điện thoại: (+84) 939111333
-            </p>
           </div>
-          <div class="col-4">
+          <div class="col-1 col-sm-4">
           </div>
-          <div class="col-4 mx-auto mb-md-0 mb-4">
+          <div class="col-7 col-sm-4 mx-auto mb-md-0 mb-4">
             <!-- Links -->
             <h6 class="text-uppercase fw-bold mb-4">Liên Hệ</h6>
             <p><i class="fas fa-home me-3"></i>Khu II - Đại học Cần Thơ</p>
