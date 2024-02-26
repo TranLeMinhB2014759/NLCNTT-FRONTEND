a6e1d204dd34bcc783b88d5829d6b3bab4612a65
TranLeMinhB2014759 <minhb2014759@student.ctu.edu.vn>
2024-02-26 23:07:09 +0700

    Add btn scrolltoTop

---
+++ src/components/Base/Footer.vue
@@ -0,0 +1,87 @@
+<template>
+  <!-- Footer -->
+  <footer class="text-center text-lg-start bg-light text-muted">
+    <!-- Section: Social media -->
+
+
+    <!-- Section: Links  -->
+    <section class="footer">
+      <div class="container text-center text-md-start mt-5">
+        <!-- Grid row -->
+        <div class="row mt-3">
+          <!-- Grid column -->
+          <div class="col-4 mx-auto mb-4">
+            <!-- Content -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              DERMATOLOGY CLINIC
+
+            </h6>
+            <p>
+              Địa chỉ: Thới Bình 1, phường Thuận An, quận Thốt Nốt, Cần Thơ
+            </p>
+            <p>
+              Điện thoại: (+84) 939111333
+            </p>
+          </div>
+          <div class="col-4">
+          </div>
+          <div class="col-4 mx-auto mb-md-0 mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">Liên Hệ</h6>
+            <p><i class="fas fa-home me-3"></i>Khu II - Đại học Cần Thơ</p>
+            <p>
+              <i class="fas fa-envelope me-3"></i>
+              minhb2014759@student.ctu.edu.vn
+            </p>
+            <p><i class="fas fa-phone me-3"></i> (+84) 939111333</p>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+      </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div class="text-center copyright p-4">
+      <p class="copyright-text">Copyright &copy; 2023 All Rights Reserved by
+        <a href="#">Master Minh</a>.
+      </p>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
+</template>
+    
+<style scoped>
+.footer {
+  padding: 10px;
+  color: #333;
+  background: #41a4ff;
+}
+
+.copyright{
+  background: rgb(39,73,203);
+  background: linear-gradient(0deg, rgba(39,73,203,1) 0%, rgba(41,157,255,1) 45%, rgba(64,164,255,1) 98%, rgba(0,0,0,1) 100%);
+}
+
+.dvtv {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 250px;
+
+}
+
+.dvtv div {
+  flex: 1;
+  text-align: center;
+}
+
+.dvtv img {
+  width: 60%;
+  height: auto;
+  margin-right: 10px;
+}
+</style>
+    
