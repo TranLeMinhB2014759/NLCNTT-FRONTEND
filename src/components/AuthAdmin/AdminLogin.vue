42bb597e5de13d9c5db6ca461ec14b75fb0d8dcd
TranLeMinhB2014759 <minhb2014759@student.ctu.edu.vn>
2024-05-01 18:32:52 +0700

    fix: login

--- src/components/AuthAdmin/AdminLogin.vue
+++ src/components/AuthAdmin/AdminLogin.vue
@@ -1,4 +1,5 @@
 <template>
+    <FormLogin @submit:login="loginStaff" />
     <main class="d-flex w-100">
         <div class="video-background">
             <video autoplay muted loop>
@@ -50,9 +51,7 @@
                                               </div>
                                           </div> -->
                                         <div class="d-grid gap-2 mt-3">
-
                                             <button class="btn btn-lg btn-primary" type="submit">Sign in</button>
-
                                         </div>
                                     </Form>
                                 </div>
@@ -69,13 +68,19 @@
         </div>
     </main>
 </template>
+
 <script>
 import { ErrorMessage, Field, Form } from "vee-validate";
-
+import FormLogin from "@/components/AuthAdmin/LoginForm.vue";
+import StaffService from "@/services/staff.service.js";
+import { toast } from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
+import Swal from 'sweetalert2'
 import * as yup from "yup";
 
 export default {
     components: {
+        FormLogin,
         Field,
         Form,
         ErrorMessage,
@@ -85,17 +90,18 @@ export default {
         const loginFormSchema = yup.object().shape({
             email: yup
                 .string()
-                .required("Email phải có giá trị.")
+                .required("Hãy nhập vào email.")
                 .email("E-mail không đúng.")
                 .max(50, "E-mail tối đa 50 ký tự."),
 
             password: yup
                 .string()
-                .required("Mật khẩu phải có giá trị.")
+                .required("Hãy nhập vào mật khẩu.")
                 .min(5, "Mật khẩu phải ít nhất 5 ký tự.")
         });
 
         return {
+            message: "",
             loginLocal: {
                 email: "",
                 password: "",
@@ -108,6 +114,43 @@ export default {
     },
 
     methods: {
+        async loginStaff(data) {
+            try {
+                const response = await StaffService.login(data);
+                var staffDataToSave = {
+                    name: response.staff.name,
+                    role: response.staff.role,
+                    email: response.staff.email,
+                    imgURL: response.staff.imgURL,
+                    _id: response.staff._id
+                };
+                localStorage.setItem('staff', JSON.stringify(staffDataToSave));
+                if (response && response.message === 'Đăng nhập thành công') {
+                    if (response.staff.role === 'admin') {
+                        this.message = "Welcome Back, " + response.staff.name;
+                        Swal.fire({
+                            icon: "success",
+                            title: this.message,
+                            showConfirmButton: true,
+                            timer: 2000
+                        });
+                        this.$router.push({ name: 'welcome' });
+                    } else {
+                        this.message = "Welcome Back, " + response.staff.name;
+                        Swal.fire({
+                            icon: "success",
+                            title: this.message,
+                            showConfirmButton: true,
+                            timer: 2000
+                        });
+                        this.$router.push({ name: 'welcome' });
+                    }
+                }
+            } catch (error) {
+                this.message = "Đăng nhập thất bại, email hoặc mật khẩu chưa chính xác";
+                toast.error(this.message);
+            }
+        },
         togglePasswordVisibility() {
             this.passwordType = this.passwordType === "password" ? "text" : "password";
             this.passwordIcon = this.passwordIcon === "fa fa-eye" ? "fa-solid fa-eye-slash" : "fa fa-eye";
